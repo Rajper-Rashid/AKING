9df6af48570b0f21d1be324316a3e0b46e601824
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-03-29 13:05:18 +0500

    Update Ok.py

--- Ok.py
+++ Ok.py
@@ -1,18 +1,16 @@
-import os, platform,time
+import os, platform
+
 try:
    import requests
+
 except:
    os.system('pip2 install requests')
-from time import sleep
+
 import requests
+
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from bboy import readline___Public_Xml
-    print("\n Congratulations! Your device supported!\n")
-    time.sleep(3)
-    readline___Public_Xml()
+    from AKING import check
+    check()
 elif bit == '32bit':
-    from f32 import readline___Public_Xml
-    print("\n Sorry Brother Your Device Not Support This Tolls!\n")
-    time.sleep(3)
-    readline___Public_Xml()
+    print('YOUR PHONE IS NOT SUPPORTED BRO')
