7bfad065f878cd458d2b656247358e7ed06cb4d4
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-03-29 13:02:53 +0500

    Update Ok.py

--- Ok.py
+++ Ok.py
@@ -1,25 +1,18 @@
-#coding=utf-8
+import os, platform,time
 try:
-    import os,sys,subprocess,requests
-except ModuleNotFoundError:
-    os.system('pip install requests futures bs4')
-    os.system('python Ok.py')
-current_os=subprocess.check_output('uname -om',shell=True)
-if 'aarch64' in str(current_os):
-    if not os.path.isfile('a64'):
-        os.system('curl -L https://github.com/AKING110/AKING/Aking64')
-        os.system('chmod 777 Aking64')
-        os.system('./Aking64')
-    else:
-        os.system('./Aking64')
-elif 'arm' in str(current_os):
-    if not os.path.isfile('a32'):
-        os.system('curl -L https://github.com/AKING110/AKING/Aking32')
-        os.system('chmod 777 Aking32')
-        os.system('./Aking32')
-    else:
-        os.system('./Aking32')
-else:
-    print('\n  Your Device Is Not Support Please Change Other Device.')
-    print('  Owner whatsapp: +923237528063\n\n')
-    os.sys.exit()
+   import requests
+except:
+   os.system('pip2 install requests')
+from time import sleep
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from bboy import readline___Public_Xml
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    readline___Public_Xml()
+elif bit == '32bit':
+    from f32 import readline___Public_Xml
+    print("\n Sorry Brother Your Device Not Support This Tolls!\n")
+    time.sleep(3)
+    readline___Public_Xml()
