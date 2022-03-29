d4b5d3a32433670a4be93d46c423a48747466a15
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-03-29 13:57:17 +0500

    Update Brand.py

--- Brand.py
+++ Brand.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from pc import Aking
+    from pc import readline___Public_Xml
     readline___Public_Xml()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
