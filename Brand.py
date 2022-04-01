fe7034eed5d87f6c1caf4033b638ef6c98e8f7b3
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-04-01 14:04:58 +0500

    Update Brand.py

--- Brand.py
+++ Brand.py
@@ -8,6 +8,6 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     from pc import main_apv
-    main_apv)
+    main_apv()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
