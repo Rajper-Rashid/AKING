4f48b63c4ed4224058cb82c4b8ebb7b7748bb74a
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-04-29 18:09:28 +0500

    Update and rename Aking.py to AKING.py

--- AKING.py
+++ AKING.py
@@ -8,7 +8,7 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     print("\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
-    from Aking import Subscription
+    from AKING import Subscription
     Subscription()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
