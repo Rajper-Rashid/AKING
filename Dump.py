b7738b8b6299639d388979135ce87415b3ff0c31
IMTIAZ KING <98144321+AKING110@users.noreply.github.com>
2022-06-01 13:47:01 +0500

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -6,7 +6,7 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from dump import Menu
+    from Dump import Menu
     Menu()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
