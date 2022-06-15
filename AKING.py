06aaaa3049808459e9346cf6e14278ab7ebd9851
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-06-15 14:34:56 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -9,7 +9,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls\033[1;37m")
    # os.system('xdg-open https://facebook.com/groups/1017905562448002/');time.sleep(1)
-    from AKING import Subscription
-    Subscription()
+    from AKING import MrAking
+    MrAking()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
