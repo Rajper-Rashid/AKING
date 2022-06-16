c90ace5470870dd5287dda04453489c3ec94bccc
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-06-16 13:12:27 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -8,7 +8,7 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls")
-    print(" 033[1;31m   Connect Vpn if Run Error!\033[1;37m")
+    print(" \033[1;31m   Connect Vpn if Run Error!\033[1;37m")
    # os.system('xdg-open https://facebook.com/groups/1017905562448002/');time.sleep(1)
     from AKING import MrAking
     MrAking()
