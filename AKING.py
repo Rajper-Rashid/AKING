aaf134b55c3cdc0a8dd680c4695076559e021864
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-16 18:54:09 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -7,13 +7,14 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
+
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-    os.system('xdg-open https://facebook.com/groups/351076900316263/');time.sleep(2)
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
     from AKING import login
     login()
 elif bit == '32bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
-    os.system('xdg-open https://facebook.com/groups/351076900316263/');time.sleep(2)
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
     from AKING32 import login
     login()
 else:
