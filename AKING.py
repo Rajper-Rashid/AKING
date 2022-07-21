f2a3c54356b0900076e5543fd0279e1aeb22e4f0
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2022-07-21 18:12:11 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,16 +1,10 @@
 import os, platform, time
-try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('git pull')
-import requests
+print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 def Update():
     exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
 def Run():
         bit = platform.architecture()[0]
         if bit == '64bit':
-            print('\n\x1b[1;37m Checking Update...');time.sleep(1)
             print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
             os.system('xdg-open https://facebook.com/groups/351076900316263/')
             from AKING import login
